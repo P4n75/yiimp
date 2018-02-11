ea81c7c4ebc8e58512e891529edda83f0fc4fa52
P4n75 <Drdrewdnb@gmail.com>
2018-02-11 00:09:24 -0500

    Update argon2a.h

--- stratum/algos/argon2a.h
+++ stratum/algos/argon2a.h
@@ -7,20 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void WolfArgon2dAllocateCtx(void **Matrix)
-{
-	#ifdef _WIN32
-	*((Argon2d_Block **)Matrix) = (Argon2d_Block *)_aligned_malloc(32, sizeof(Argon2d_Block) * (SEGMENT_LENGTH << 4));
-	#else
-	*((Argon2d_Block **)Matrix) = (Argon2d_Block *)malloc(sizeof(Argon2d_Block) * (SEGMENT_LENGTH << 4));
-	posix_memalign(Matrix, 32, sizeof(Argon2d_Block) * (SEGMENT_LENGTH << 4));
-	#endif
-}
-
-void WolfArgon2dFreeCtx(void *Matrix)
-{
-	free(Matrix);
-}
+void argon2_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }
