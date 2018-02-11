77c8710fcff0349eef10d678c3b306e5fc2443a2
P4n75 <Drdrewdnb@gmail.com>
2018-02-11 02:54:23 -0500

    Update argon2a.c

--- stratum/algos/argon2a.c
+++ stratum/algos/argon2a.c
@@ -18,123 +18,29 @@
 #define ZERO 0
 
 
-const char *argon2_type2string(argon2_type type, int uppercase) {
-    switch (type) {
-        case Argon2_d:
-            return uppercase ? "Argon2d" : "argon2d";
-    }
+inline void argon_call(void *out, void *in, void *salt, int type)
+{
+	argon2_context context = { 0 };
 
-    return NULL;
-}
-int argon2_ctx(argon2_context *context, argon2_type type) {
-    /* 1. Validate all inputs */
-    int result = validate_inputs(context);
-    uint32_t memory_blocks, segment_length;
-    argon2_instance_t instance;
+	context.out = (uint8_t *)out;
+	context.pwd = (uint8_t *)in;
+	context.salt = (uint8_t *)salt;
 
-    if (ARGON2_OK != result) {
-        return result;
-    }
-
-    if (Argon2_d != type) {
-        return ARGON2_INCORRECT_TYPE;
-    }
-
-    /* 2. Align memory size */
-    /* Minimum memory_blocks = 8L blocks, where L is the number of lanes */
-    memory_blocks = context->m_cost;
-
-    if (memory_blocks < 2 * ARGON2_SYNC_POINTS * context->lanes) {
-        memory_blocks = 2 * ARGON2_SYNC_POINTS * context->lanes;
-    }
-
-    segment_length = memory_blocks / (context->lanes * ARGON2_SYNC_POINTS);
-    /* Ensure that all segments have equal length */
-    memory_blocks = segment_length * (context->lanes * ARGON2_SYNC_POINTS);
-
-    instance.memory = NULL;
-    instance.passes = context->t_cost;
-    instance.memory_blocks = memory_blocks;
-    instance.segment_length = segment_length;
-    instance.lane_length = segment_length * ARGON2_SYNC_POINTS;
-    instance.lanes = context->lanes;
-    instance.limit = 1;
-    instance.threads = context->threads;
-    instance.type = type;
-
-    if (instance.threads > instance.limit) {
-        instance.threads = instance.limit;
-    }
-
-    /* 3. Initialization: Hashing inputs, allocating memory, filling first
-     * blocks
-     */
-    result = initialize(&instance, context);
-
-    if (ARGON2_OK != result) {
-        return result;
-    }
-
-    /* 4. Filling memory */
-    result = fill_memory_blocks(&instance);
-
-    if (ARGON2_OK != result) {
-        return result;
-    }
-    /* 5. Finalization */
-    finalize(context, &instance);
-
-    return ARGON2_OK;
-}
-int argon2d(argon2_context *context) { return argon2_core(context, Argon2_d); }
-
-
-inline int argon2_hash(const uint32_t t_cost, const uint32_t m_cost,
-                const uint32_t parallelism, const void *pwd,
-                const size_t pwdlen, const void *salt, const size_t saltlen,
-                void *hash, const size_t hashlen, char *encoded,
-                const size_t encodedlen, argon2_type type){
-
-    argon2_context context;
-    int result;
-    uint8_t *out;
-
-    context.out = (uint8_t *)out;
-    context.outlen = (uint32_t)hashlen;
-    context.pwd = CONST_CAST(uint8_t *)pwd;
-    context.pwdlen = (uint32_t)pwdlen;
-    context.salt = CONST_CAST(uint8_t *)salt;
-    context.saltlen = (uint32_t)saltlen;
-    context.secret = NULL;
-    context.secretlen = 0;
-    context.ad = NULL;
-    context.adlen = 0;
-    context.t_cost = t_cost;
-    context.m_cost = m_cost;
-    context.lanes = parallelism;
-    context.threads = parallelism;
-    context.allocate_cbk = NULL;
-    context.free_cbk = NULL;
-    context.flags = ARGON2_DEFAULT_FLAGS;
-    
-    result = argon2_ctx(&context, type);
-    
-    if (result != ARGON2_OK) {
-        clear_internal_memory(out, hashlen);
-        free(out);
-        return result;
-    }
-        
-    
-    if (hash) {
-    memcpy(hash, out, hashlen);
-    }
-   
-    clear_internal_memory(out, hashlen);
-    free(out);
-
-    return ARGON2_OK;
-
+	argon2_core(&context, type);
 }
 
-void argon2d_hash(const char* hash, char* out, uint32_t hashlen);
+void argon2_hash(const char* input, char* output, uint32_t len)
+{
+	uint32_t _ALIGN(32) hashA[8], hashB[8];
+
+	my_scrypt((unsigned char *)input, len,
+		(unsigned char *)input, len,
+		(unsigned char *)hashA);
+
+	argon_call(hashB, hashA, hashA, (hashA[0] & MASK) == ZERO);
+
+	my_scrypt((const unsigned char *)hashB, 32,
+		(const unsigned char *)hashB, 32,
+		(unsigned char *)output);
+}
+
