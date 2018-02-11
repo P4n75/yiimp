79136d2c087cebd019528642927067d1810d8c8a
P4n75 <Drdrewdnb@gmail.com>
2018-02-10 22:05:29 -0500

    Update argon2a.h

--- stratum/algos/argon2a.h
+++ stratum/algos/argon2a.h
@@ -7,41 +7,9 @@ extern "C" {
 
 #include <stdint.h>
 
-  
-#define _ALIGN(x) __attribute__ ((aligned(x)))
+void argon2_hash(const char* input, char* output, uint32_t len);
 
-#define T_COSTS 2
-#define M_COSTS 16
-#define MASK 8
-#define ZERO 0
-
-inline void argon_call(void *out, void *in, void *salt, int type)
-{
-	argon2_context context = { 0 };
-
-	context.out = (uint8_t *)out;
-	context.pwd = (uint8_t *)in;
-	context.salt = (uint8_t *)salt;
-
-	argon2_core(&context, type);
-}
-
-void argon2_hash(const char* input, char* output, uint32_t len)
-{
-	uint32_t _ALIGN(32) hashA[8], hashB[8];
-
-	my_scrypt((unsigned char *)input, len,
-		(unsigned char *)input, len,
-		(unsigned char *)hashA);
-
-	argon_call(hashB, hashA, hashA, (hashA[0] & MASK) == ZERO);
-
-	my_scrypt((const unsigned char *)hashB, 32,
-		(const unsigned char *)hashB, 32,
-		(unsigned char *)output);
-}
-
-#if defined(__cplusplus)
+#ifdef __cplusplus
 }
 #endif
 
