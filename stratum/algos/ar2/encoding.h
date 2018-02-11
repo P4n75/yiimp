4500d50397b31e2a7ebd5080bef64bdfa5397538
P4n75 <Drdrewdnb@gmail.com>
2018-02-11 11:47:13 -0500

    Update encoding.h

--- stratum/algos/ar2/encoding.h
+++ stratum/algos/ar2/encoding.h
@@ -31,9 +31,10 @@
 *
 * on success, ARGON2_OK is returned.
 */
-int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
-                  argon2_type type);
 
+/* int encode_string(char *dst, size_t dst_len, argon2_context *ctx,
+                  argon2_type type);
+*/
 /*
 * Decodes an Argon2 hash string into the provided structure 'ctx'.
 * The only fields that must be set prior to this call are ctx.saltlen and
